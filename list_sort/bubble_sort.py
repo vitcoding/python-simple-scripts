1bb1517e12699ea899e11b92453a03b6aee0b5ec
Vitaly_D <vitcode.mail@gmail.com>
2024-08-29 21:26:08 +0300

    Bubble sort addition

--- list_sort/bubble_sort.py
+++ list_sort/bubble_sort.py
@@ -13,12 +13,33 @@ def simple_bubble_sort(nums: list) -> list:
     return nums
 
 
+def bubble_sort(nums: list) -> list:
+
+    n = len(nums)
+
+    for i in range(n - 1):
+        not_permutated = True
+        for j in range(n - i - 1):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                not_permutated = False
+        if not_permutated:
+            break
+
+    return nums
+
+
 def main():
+
     nums = list(range(-50, 51))
     shuffle(nums)
     print(f"\nUnsorted list:\n{nums}")
+
     simple_bubble_sort(nums)
-    print(f"\nSorted list:\n{nums}")
+    print(f"\nSorted list (simple_bubble_sort):\n{nums}\n")
+
+    bubble_sort(nums)
+    print(f"\nSorted list (bubble_sort):\n{nums}\n")
 
 
 if __name__ == "__main__":
